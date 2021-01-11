fc2f0c718862d84df6f742b35b7fbd20353034ff
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-11 09:46:20 -0500

    added replaceAll for se tags

--- src/utilities/ConvertSETags.js
+++ src/utilities/ConvertSETags.js
@@ -1,8 +1,18 @@
 const ConvertSETags = (element) => {
   const replaceSETags = element
-    .replace("selink", "a")
-    .replace("externallink", "href")
-    .replace("seml", "div");
+    .replaceAll(`selink`, "a")
+    .replaceAll(`externallink`, "href")
+    .replaceAll(`linktype="EXTERNAL"`, "")
+    .replaceAll(`newwindow="false"`, "")
+    .replaceAll(`showtoolbar="no"`, "")
+    .replaceAll(`objectid=""`, "")
+    .replaceAll(`class=""`, "")
+    .replaceAll(`popupwidth`, "")
+    .replaceAll(`popupheight`, "")
+    .replaceAll(`title`, "")
+    .replaceAll(`localanchor`, "")
+    .replaceAll(`linkanchor`, "")
+    .replaceAll(`seml`, "div");
 
   return replaceSETags;
 };
