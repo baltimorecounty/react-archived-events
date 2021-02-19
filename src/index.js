543abf762e08b60881dead11f876a35650724383
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-02-19 10:00:30 -0500

    added polyfill for replaceAll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
+import "./polyfills/replaceAll.polyfill";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
